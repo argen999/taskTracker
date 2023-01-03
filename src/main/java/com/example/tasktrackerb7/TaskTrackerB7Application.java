bfabb77fc051b6ca39dd7f94db0a83289c9eccaf
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-01-03 14:59:16 +0600

    add files

---
+++ src/main/java/com/example/tasktrackerb7/TaskTrackerB7Application.java
@@ -0,0 +1,15 @@
+package com.example.tasktrackerb7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class TaskTrackerB7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerB7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tasktrackerb7/TaskTrackerB7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackerb7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerB7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
