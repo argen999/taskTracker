5390dd2255bafd2c434519ced40b8daee83d17ea
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-01-17 11:53:05 +0600

    change applicatication properties file

--- src/main/java/com/example/tasktrackerb7/configs/swagger/SwaggerConfig.java
+++ src/main/java/com/example/tasktrackerb7/configs/swagger/SwaggerConfig.java
@@ -13,7 +13,7 @@ import java.util.Collections;
 @Configuration
 public class SwaggerConfig {
 
-    private static final String API_KEY = "Bearer Token";
+    private static final String API_KEY = "Bearer Token ";
 
     @Bean
     public OpenAPI customOpenAPI() {
