b5f78e5c98f9d83a41edc3fca610350a0558b44c
user <abdymomunovargen6@gmail.com>
2023-01-24 14:51:16 +0600

    ColumnRequest and ColumnResponse create

--- src/main/java/com/example/tasktrackerb7/dto/request/ColumnRequest.java
+++ src/main/java/com/example/tasktrackerb7/dto/request/ColumnRequest.java
@@ -1,4 +1,14 @@
 package com.example.tasktrackerb7.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class ColumnRequest {
+
+    @NotNull(message = "Column cannot be unnamed!")
+    @NotBlank(message = "Name cannot be empty!")
+    @Size(min = 3, message = "Column name cannot be too short!")
+    @Size(max = 30, message = "Column name cannot be too long!")
+    private String name;
 }
--- src/main/java/com/example/tasktrackerb7/dto/response/ColumnResponse.java
+++ src/main/java/com/example/tasktrackerb7/dto/response/ColumnResponse.java
@@ -1,4 +1,18 @@
 package com.example.tasktrackerb7.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
 public class ColumnResponse {
+    private Long id;
+
+    private String name;
+
+    public ColumnResponse(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
