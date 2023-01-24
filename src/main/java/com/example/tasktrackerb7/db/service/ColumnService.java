34aadf64d6e9a65dce4c75d831cc9935e77ea44f
user <abdymomunovargen6@gmail.com>
2023-01-24 14:58:52 +0600

    ColumnRepository and ColumnService create

---
+++ src/main/java/com/example/tasktrackerb7/db/service/ColumnService.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackerb7.db.service;
+
+import com.example.tasktrackerb7.dto.request.ColumnRequest;
+import com.example.tasktrackerb7.dto.response.ColumnResponse;
+
+import java.util.List;
+
+public interface ColumnService {
+    ColumnResponse create(Long boardId, ColumnRequest columnRequest);
+    ColumnResponse update(Long id, ColumnRequest columnRequest);
+    ColumnResponse delete(Long id);
+    List<ColumnResponse> getAll(Long boardId);
+}
--- src/main/java/com/example/tasktrackerb7/dto/request/ColumnRequest.java
+++ src/main/java/com/example/tasktrackerb7/dto/request/ColumnRequest.java
@@ -1,9 +1,14 @@
 package com.example.tasktrackerb7.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Setter
+@Getter
 public class ColumnRequest {
 
     @NotNull(message = "Column cannot be unnamed!")
