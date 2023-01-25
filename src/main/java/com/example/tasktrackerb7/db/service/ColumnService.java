220551992c022c6196f8a8f15c7f758f578197ae
user <abdymomunovargen6@gmail.com>
2023-01-25 17:55:54 +0600

    Command Option L does

--- src/main/java/com/example/tasktrackerb7/db/service/ColumnService.java
+++ src/main/java/com/example/tasktrackerb7/db/service/ColumnService.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 public interface ColumnService {
     ColumnResponse create(Long boardId, ColumnRequest columnRequest);
+
     ColumnResponse update(Long id, ColumnRequest columnRequest);
+
     ColumnResponse delete(Long id);
+
     List<ColumnResponse> getAll(Long boardId);
 }
