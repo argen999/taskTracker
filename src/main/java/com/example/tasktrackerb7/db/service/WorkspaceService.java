59e709bcd46442fa10633a2d4ddfd96dc4ec0617
Aruuk <skybanychnek@gmail.com>
2023-01-26 15:22:10 +0600

    add service, add @Override in WorkspaceServiceImpl

--- src/main/java/com/example/tasktrackerb7/db/service/WorkspaceService.java
+++ src/main/java/com/example/tasktrackerb7/db/service/WorkspaceService.java
@@ -1,11 +1,22 @@
 package com.example.tasktrackerb7.db.service;
 
 import com.example.tasktrackerb7.dto.request.WorkspaceRequest;
+import com.example.tasktrackerb7.dto.response.SimpleResponse;
 import com.example.tasktrackerb7.dto.response.WorkspaceResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface WorkspaceService {
 
     WorkspaceResponse create(WorkspaceRequest workspaceRequest);
+
+    SimpleResponse delete(Long id);
+
+    WorkspaceResponse update(Long id, WorkspaceRequest workspaceRequest);
+
+    WorkspaceResponse getById(Long id);
+
+    List<WorkspaceResponse> getAll();
 }
