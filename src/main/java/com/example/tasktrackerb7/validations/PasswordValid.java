211c829b7420902fe078f1501fc4e9da85363231
Aruuk <skybanychnek@gmail.com>
2023-02-11 18:31:08 +0600

    update @Password annotation condition

--- src/main/java/com/example/tasktrackerb7/validations/PasswordValid.java
+++ src/main/java/com/example/tasktrackerb7/validations/PasswordValid.java
@@ -1,6 +1,5 @@
 package com.example.tasktrackerb7.validations;
 
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
@@ -8,10 +7,7 @@ public class PasswordValid implements ConstraintValidator<Password, String> {
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
-        if (password.length() > 6 && password.length() < 30) {
-            return password.matches("^(?=.*[a-z])(?=\\\\S+$).{6,20}$");
-        } else {
-            return false;
-        }
+        return password.length() > 6 && password.length() < 30;
     }
+
 }
