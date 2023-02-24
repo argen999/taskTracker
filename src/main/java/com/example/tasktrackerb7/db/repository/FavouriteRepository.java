8693d3eb498130ae30b1825eeecf872acfa0388a
user1 <maha279@gmail.com>
2023-02-24 16:57:34 +0600

    added board id and workspace id

--- src/main/java/com/example/tasktrackerb7/db/repository/FavouriteRepository.java
+++ src/main/java/com/example/tasktrackerb7/db/repository/FavouriteRepository.java
@@ -13,10 +13,10 @@ import java.util.List;
 @Repository
 public interface FavouriteRepository extends JpaRepository<com.example.tasktrackerb7.db.entities.Favourite, Long> {
 
-    @Query("select new com.example.tasktrackerb7.dto.response.FavouriteBoardResponse(f.id, f.board.name,f.user.photoLink) from Favourite f where f.user.id =:id  and f.isBoard = false ")
+    @Query("select new com.example.tasktrackerb7.dto.response.FavouriteBoardResponse(f.id,f.board.id, f.board.name,f.user.photoLink) from Favourite f where f.user.id =:id  and f.isBoard = false ")
     List<FavouriteBoardResponse> getFavouriteByIsBoard(Long id);
 
-    @Query("select new com.example.tasktrackerb7.dto.response.FavouriteWorkspaceResponse(f.id, f.workspace.name) from Favourite f where f.user.id =:id  and f.isBoard =true")
+    @Query("select new com.example.tasktrackerb7.dto.response.FavouriteWorkspaceResponse(f.id,f.workspace.id, f.workspace.name) from Favourite f where f.user.id =:id  and f.isBoard =true")
     List<FavouriteWorkspaceResponse> getWorkspaceByIsFavourite(Long id);
 
     @Query(value = """
